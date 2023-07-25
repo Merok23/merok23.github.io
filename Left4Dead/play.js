f603261c438a76f23a00d2463e8d1ef82a3ea779
Merok23 <alejocalizblanco@gmail.com>
2023-07-25 11:27:32 -0300

    Se agregaron mas wallpapers a la galeria, y se agrego que el boton de download en play cuando se clickea, se mate al zombie y quede en el piso hasta que se recargue la pagina.

---
+++ Left4Dead/play.js
@@ -0,0 +1,7 @@
+function loadFinalImage() {
+    // Get the button element
+    var downloadButton = document.querySelector('.download-button');
+    
+    // Add a class to the button on click
+    downloadButton.classList.add('final-image-loaded');
+  }
