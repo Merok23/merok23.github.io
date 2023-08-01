35aceccc024b289249c34f7883f8b4a5600b13d2
Merok23 <alejocalizblanco@gmail.com>
2023-08-01 15:42:03 -0300

    Se cambio el script de descargar a un nuevo js porque me parece que no lo incluye en github.

---
+++ Left4Dead/download_installer.js
@@ -0,0 +1,13 @@
+
+function downloadFile() {
+    // Fetch the file URL from the server (replace this with your server endpoint)
+    fetch("https://raw.githubusercontent.com/Merok23/TPG-Taller-Left4dead-Grupo1/main/installer.sh")
+      .then(response => response.text())
+      .then(fileURL => {
+        var link = document.createElement("a");
+        link.href = fileURL;
+        link.setAttribute("download", "installer.sh");
+        link.click();
+      })
+      .catch(error => console.error("Error fetching the download URL:", error));
+  }
