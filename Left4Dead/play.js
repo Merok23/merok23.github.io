04ffee111baebeb2b8255d951ca828e9073e27f9
Merok23 <alejocalizblanco@gmail.com>
2023-08-01 15:34:32 -0300

    Se agregaron fotos del menu en game screenshots, media y se cambio el boton de download para que con un script de js descargue el archivo en vez de enviarte a la pagina.

--- Left4Dead/play.js
+++ Left4Dead/play.js
@@ -5,3 +5,16 @@ function loadFinalImage() {
     // Add a class to the button on click
     downloadButton.classList.add('final-image-loaded');
   }
+
+function downloadFile() {
+  // Fetch the file URL from the server (replace this with your server endpoint)
+  fetch("https://raw.githubusercontent.com/Merok23/TPG-Taller-Left4dead-Grupo1/main/installer.sh")
+    .then(response => response.text())
+    .then(fileURL => {
+      var link = document.createElement("a");
+      link.href = fileURL;
+      link.setAttribute("download", "installer.sh");
+      link.click();
+    })
+    .catch(error => console.error("Error fetching the download URL:", error));
+}
